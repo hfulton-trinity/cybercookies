5e0d4e8f0068545a17759439b1da36254d7ee73c
Hans Fulton <hfulton-trinity>
2021-05-04 22:15:53 -0500

    small route view and controller changes

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -3,14 +3,14 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import play.api.i18n._
-import models.MsgInMemoryModel
+//import models.MsgInMemoryModel
 import models._
 import play.api.libs.json._
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def load = { implicit request =>
+  def load = Action{ implicit request =>
     Ok(views.html.home())
   }
 
