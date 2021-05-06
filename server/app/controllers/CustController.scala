fb62af7ba76c83de35772f7ebf261eeb4d1ef2cf
emiller6 <emiller6@trinity.edu>
2021-05-06 14:46:03 -0500

    More separation, routes added

---
+++ server/app/controllers/CustController.scala
@@ -0,0 +1,59 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.i18n._
+import models.UserModel
+import models.TroopModel
+import models._
+import play.api.libs.json._
+
+@Singleton
+class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def load = Action{ implicit request =>
+    Ok(views.html.cust())
+  }
+
+  //Figure out database interactions
+
+  implicit val custDataReads = Json.reads[CustData]
+  implicit val troopDataReads = Json.reads[TroopData]
+
+  def withJsonBody[A](f: A => Result)(implicit request: Request[AnyContent], reads: Reads[A]) = {
+    request.body.asJson.map { body =>
+      Json.fromJson[A](body) match {
+        case JsSuccess(a,path) => f(a)
+        case e @ JsError(_) => Redirect(routes.Application.load())
+      }
+    }.getOrElse(Redirect(routes.Application.load()))
+
+  }
+
+  def withSessionUsername(f: String => Result)(implicit request: Request[AnyContent]) = {
+    request.session.get("username").map(f).getOrElse(Ok(Json.toJson(Seq.empty[String])))
+  }
+
+  def validateCustomer = Action { implicit request =>
+    withJsonBody[CustData] {ud =>
+      if(UserModel.logIn(ud.username, ud.password)){
+        Ok(Json.toJson(true))
+          .withSession("username" -> ud.username, "csrfToken" -> play.filters.csrf.CSRF.getToken.map(_.value).getOrElse(""))
+      } else {
+        Ok(Json.toJson(false))
+      }
+    }
+  }
+
+  def newCustomer = Action { implicit request =>
+  //NEEDS WORK
+    withJsonBody[CustData] {ud =>
+      if(UserModel.logIn(ud.username, ud.password)){
+        Ok(Json.toJson(true))
+          .withSession("username" -> ud.username, "csrfToken" -> play.filters.csrf.CSRF.getToken.map(_.value).getOrElse(""))
+      } else {
+        Ok(Json.toJson(false))
+      }
+    }
+  }
+}
--- server/app/controllers/TroopController.scala
+++ server/app/controllers/TroopController.scala
@@ -1,2 +1,24 @@
 package controllers
 
+def newTroop = Action {
+//NEEDS WORK
+  withJsonBody[TroopData] {ud =>
+    if(TroopModel.logIn(ud.no, ud.password)){
+      Ok(Json.toJson(true))
+        .withSession("no" -> ud.no, "csrfToken" -> play.filters.csrf.CSRF.getToken.map(_.value).getOrElse(""))
+    } else {
+      Ok(Json.toJson(false))
+    }
+  }
+}
+
+def validateTroop = Action {
+  withJsonBody[TroopData] {ud =>
+    if(TroopModel.logIn(ud.no, ud.password)){
+      Ok(Json.toJson(true))
+        .withSession("no" -> ud.no, "csrfToken" -> play.filters.csrf.CSRF.getToken.map(_.value).getOrElse(""))
+    } else {
+      Ok(Json.toJson(false))
+    }
+  }
+}
